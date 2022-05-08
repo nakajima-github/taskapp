5ffff99069b6b1486e7d6def03aecddd3fbf79c0
Nakajima <n_j5192@yahoo.co.jp>
2022-05-08 18:37:09 +0900

    pod installと.gitignoreの作成まで実施

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // tableViewの追加設定
+        // データを表示していない部分に罫線を表示する
+        tableView.fillerRowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // データの数（＝セルの数）を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能なcellを得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    // 各セルを選択したときに実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //UITableViewDelegateプロトコルのメソッドで、セルをタップした時にタスク入力画面に遷移させる
+        performSegue(withIdentifier: "cellSegue", sender: nil)
     }
 
-
+    // セルの削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        // UITableViewDelegateプロトコルのメソッドで、セルが削除可能なことを伝える
+        return .delete
+    }
+    
+    // Deleteボタンが押されたときに呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // UITableViewDataSourceプロトコルのメソッドで、Deleteボタンが押されたときにローカル通知をキャンセルし、データベースからタスクを削除する
+        
+    }
 }
 
